102fe120d5ac182c058b6cc79396d9e3e1d26373
Francesco De Vivo <francescodvf@yahoo.it>
2025-11-01 18:14:12 +0100

    Migrating from Sequelize to MongoDB

--- models/product.js
+++ models/product.js
@@ -1,61 +1,23 @@
-const { DataTypes } = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+const getDb = require('../util/database').getDb;
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+  save() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = Product;
-
-// const db = require('../util/database');
-
-// const Cart = require('./cart');
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     return db.execute(
-//       'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-//       [this.title, this.price, this.imageUrl, this.description]
-//     );
-//   }
-
-//   static deleteById(id) {}
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM products');
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-//   }
-// };
